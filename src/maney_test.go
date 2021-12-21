cf7b3f2b4e5d525bddf79375142539c51f32674c
nao-18 <kyahyo17@yahoo.co.jp>
2021-12-21 21:14:58 +0900

    separate to package portfolio

--- src/maney_test.go
+++ src/maney_test.go
@@ -46,18 +46,3 @@ func assertEqual(t *testing.T, expected Money, actual Money) {
 		t.Errorf("Expected %+v Got %+v", expected, actual)
 	}
 }
-
-type Portfolio []Money
-
-func (p Portfolio) Add(money Money) Portfolio {
-	p = append(p, money)
-	return p
-}
-
-func (p Portfolio) Evaluate(currency string) Money {
-	total := 0.0
-	for _, m := range p {
-		total = total + m.amount
-	}
-	return Money{amount: total, currency: currency}
-}
