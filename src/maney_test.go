5083f6890738932e113d092f13488c8ba9695c56
nao-18 <kyahyo17@yahoo.co.jp>
2021-12-21 21:12:46 +0900

    separate to package money

--- src/maney_test.go
+++ src/maney_test.go
@@ -47,19 +47,6 @@ func assertEqual(t *testing.T, expected Money, actual Money) {
 	}
 }
 
-type Money struct {
-	amount   float64
-	currency string
-}
-
-func (m Money) Times(multiplier int) Money {
-	return Money{amount: m.amount * float64(multiplier), currency: m.currency}
-}
-
-func (m Money) Divide(divisor int) Money {
-	return Money{amount: m.amount / float64(divisor), currency: m.currency}
-}
-
 type Portfolio []Money
 
 func (p Portfolio) Add(money Money) Portfolio {
